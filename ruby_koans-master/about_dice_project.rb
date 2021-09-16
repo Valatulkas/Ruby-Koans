4debb6cdbd0c41c3dee5df25fac7b99093e4fa00
jmferg11 <jfergie93@gmail.com>
2021-09-16 16:14:54 -0400

    finish ruby koans!

--- ruby_koans-master/about_dice_project.rb
+++ ruby_koans-master/about_dice_project.rb
@@ -2,9 +2,22 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_accessor :values
+  
+  def initialize
+    self.values = []
+  end
+  def roll(x)
+    self.values = []
+    possibilities = [1, 2, 3, 4, 5, 6]
+    result = possibilities[(rand(5))]
+    x.times.each do
+      self.values << result
+    end
+    self.values
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
